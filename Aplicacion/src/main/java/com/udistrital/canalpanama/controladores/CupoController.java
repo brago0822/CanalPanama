22ef58c00418336d43e6ca08dbf0b6770f0d60b5
Brago0822 <brago0822@gmail.com>
2019-03-06 00:00:12 -0500

    Login funcionando, index carga con AdminLTE ok

--- Aplicacion/src/main/java/com/udistrital/canalpanama/controladores/CupoController.java
+++ Aplicacion/src/main/java/com/udistrital/canalpanama/controladores/CupoController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.udistrital.canalpanama.daos.CupoDAO;
 import com.udistrital.canalpanama.util.ODBManager;
